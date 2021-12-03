d4b357db52c53ef3ce6bc47eaf50c1d6d06a60f6
Nixa F <niksa.fantela@talentlyft.com>
2021-12-03 13:08:10 +0100

    Added interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpLoadingInterceptor } from './core/interceptors/http-loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpLoadingInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/interceptors/http-loading.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { GlobalHttpLoaderService } from '../services/global-http-loader.service';
+import { GlobalLoaderCorrectLoaderUrlFormat, GlobalLoaderHttpMethods } from '../models/global-http-loader.models';
+
+@Injectable()
+export class HttpLoadingInterceptor implements HttpInterceptor {
+  constructor(private readonly _loaderService: GlobalHttpLoaderService) { }
+
+  private readonly _supportedHTTPMethods: ReadonlyArray<GlobalLoaderHttpMethods> = ['POST', 'GET', 'DELETE', 'PUT', 'PATCH'];
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const method = this._supportedHTTPMethods.find(m => m === req.method);
+    const requiredUrl: GlobalLoaderCorrectLoaderUrlFormat = method ? `${method}-${req.url}` : '';
+    if (requiredUrl) {
+      this._loaderService.nextRequestUrl(requiredUrl);
+    }
+
+    return next.handle(req).pipe(
+      finalize(() => {
+        requiredUrl && this._loaderService.nextRequestUrl(requiredUrl);
+      }),
+    );
+  }
+}
